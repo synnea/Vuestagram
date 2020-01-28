9e945b834e9856d71ed683989359f6afde1abb2b
Carina <carrie.poell@gmail.com>
2020-01-28 14:08:35 +0100

    feat: render the 20 latest posts

--- src/components/InstagramFeed.vue
+++ src/components/InstagramFeed.vue
@@ -4,9 +4,11 @@
 
   <h3>Instagram Feed</h3>
 
-  <div v-for="post in imagePosts" v-bind:key="post.id">
+    <div v-for="post in renderPosts" v-bind:key="post.id">
+
 {{ post }}
-    </div>
+
+      </div>
 
     <section v-if="errored">
     <p>We're sorry, we're not able to retrieve this information at the moment,
@@ -22,6 +24,8 @@
 
 <script>
   import axios from 'axios';
+  import _ from 'lodash'; 
+
   export default {
     name: 'InstagramFeed',
   data () {
@@ -33,12 +37,40 @@
     }
   },
 
-  created () {
+  mounted () {
       this.fetchPosts();
+  },
+
+  computed: {
+    renderPosts: function () {
+      // receives the image and video posts, concatenates, cleans, orders and returns them.
+        if (this.videoPosts.length && this.imagePosts.length) {
+          let allPosts = this.imagePosts.concat(this.videoPosts)
+          let allPostsShelled = allPosts.map(obj => obj.node )
+          let allPostsOrdered = _.orderBy(allPostsShelled, ["taken_at_timestamp"], ["desc"]).slice(0,20)
+          console.log(allPostsOrdered)
+          return allPostsOrdered
+
+          // if either video or image post arrays are empty, return only the values for the existing array.
+          // Note: this function is needlessly bloated and needs refactoring. This method was chosen as a temporary solution due to time constraints.       
+          } else if (this.videoPosts.length && !this.imagePosts.length) {
+          videoPostsShelled = videoPosts.map(obj => obj.node)
+          let allVideoPostsOrdered = _.orderBy(videoPostsShelled, ["taken_at_timestamp"], ["desc"]).slice(0,20)
+          return allVideoPostsOrdered
+        } else if (!this.videoPosts.length && this.imagePosts.length) {
+          imagePostsShelled = imagePosts.map(obj => obj.node)
+          let allImagePostsOrdered = _.orderBy(imagePostsShelled, ["taken_at_timestamp"], ["desc"]).slice(0,20)
+          return allImagePostsOrdered
+        }
+        else {
+          return this.raiseError()
+        }
+      },    
   },
 
   methods: {
         fetchPosts() {
+              // makes the API call and saves the response.
     axios
         .get('http://localhost:8080/artnight.json')
         .then((response) => {
@@ -51,6 +83,11 @@
       .finally(() => this.loading = false);
 
           },
+
+      raiseError() {
+        // gets called if both video and image post arrays were returned empty.
+        this.errored = true;
+      }
   }
   }
 
